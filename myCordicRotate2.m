8d569869017009d6d0b77282f8d5b7108beb8baf
Chi-Sheng Yang <b03901101@ntu.edu.tw>
2019-08-28 14:56:29 -0400

    Update myCordicRotate2.m

--- myCordicRotate2.m
+++ myCordicRotate2.m
@@ -1,7 +1,6 @@
 function v = myCordicRotate2(theta, u, niters)
     % Hyperbolic and Rotation mode    
     x = real(u); y = imag(u);
-    k = 4; % Used for (3k+1) iterations steps
     for iter=1:niters
         angle = atanh(2^(-iter));
         if (theta > 0)
@@ -15,21 +14,6 @@ function v = myCordicRotate2(theta, u, niters)
         end
         x = new_x;
         y = new_y;
-        
-        if (iter==k)
-            if (theta > 0)
-                new_x = x + y*(2^(-iter));
-                new_y = y + x*(2^(-iter));
-                theta = theta - angle;
-            else
-                new_x = x - y*(2^(-iter));
-                new_y = y - x*(2^(-iter));
-                theta = theta + angle;
-            end
-            x = new_x;
-            y = new_y;
-            k = 3*k+1;
-        end
         
     end
     
